05aef9fa96055ba4b12bc9ec410d787092539669
BrunaCardoso7 <obitadrawing@mgmail.com>
2024-05-26 01:37:27 -0300

    fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { createUserController, loginUserController } from "../controllers/userController.js"
+import { createUserController, loginUserController, userById } from "../controllers/userController.js"
 
 const userRoute = express.Router()
 
 userRoute.post('/create', createUserController)
 userRoute.post('/login', loginUserController)
+userRoute.get('/:id', userById)
 export default userRoute
